ac21bee8cb04b30473daa9f15343d815167c7ffd
Marius Rieder <marius.rieder@durchmesser.ch>
2020-01-18 19:47:02 +0100

    Build support for bakery.

--- web/plugins/wato/agent_bakery_winnfs.py
+++ web/plugins/wato/agent_bakery_winnfs.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-register_rule(
-    "agents/" + _("Agent Plugins"),
-    "agent_config:winnfssrv",
-    Alternative(
-        title = _("Windows NFS Server"),
-        help = _("This will deploy the agent plugin <tt>winnfs</tt> "
-                 "for checking Windows NFS Server and Shares.")
-        style = "dropdown",
-        elements = [
-	    FixedValue(True, title _("Deploy the Windows NFS Server plugin"), totext = _("(enabled)") ),
-            FixedValue(None, title = _("Do not deploy the SSL certificates plugin"), totext = _("(disabled)") ),
-        ]
-    ),
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    DropdownChoice,
 )
+from cmk.gui.plugins.wato import (
+    HostRulespec,
+    rulespec_registry,
+)
+
+try:
+    from cmk.gui.cee.plugins.wato.agent_bakery import RulespecGroupMonitoringAgentsWindowsAgent
+except:
+    from cmk.gui.plugins.wato import RulespecGroupCheckParametersStorage as RulespecGroupMonitoringAgentsWindowsAgent
+
+def _valuespec_agent_config_winnfs():
+    return DropdownChoice(
+        title=_("Windows NFS Server"),
+        help=_("This will deploy the agent plugin <tt>winnfs</tt> "
+               "for checking Windows NFS Server and Shares."),
+        choices=[
+            (True, _("Deploy Windows NFS Server plugin")),
+            (None, _("Do not deploy Windows NFS Server plugin")),
+        ],
+    )
+
+rulespec_registry.register(
+    HostRulespec(
+        group=RulespecGroupMonitoringAgentsWindowsAgent,
+        name="agent_config:winnfs",
+        valuespec=_valuespec_agent_config_winnfs,
+    ))
