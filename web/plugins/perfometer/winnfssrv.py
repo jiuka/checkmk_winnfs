c85a3fcf3ee76c7a095b493ecc29bf7e8e8a65eb
Marius Rieder <marius.rieder@scs.ch>
2020-01-20 15:28:56 +0100

    Update perfometer to split view.

--- web/plugins/perfometer/winnfssrv.py
+++ web/plugins/perfometer/winnfssrv.py
@@ -2,7 +2,15 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
 def perfometer_winnfssrv(row, check_command, perf_data):
-    perf_data = dict(map(lambda x: (x[0], x[1:]), perf_data))
-    return "Clients: %d" % (perf_data['winnfssrv_client'][0]), perfometer_logarithmic(perf_data['winnfssrv_client'][0], 4, 2, "#0f0")
+    h = '<div class="stacked">'
+    texts = []
+    for i, color, base, scale, verbfunc in [
+        (0, "#00FFB2", 10000, 10, lambda v: ("Clients: %d") % v),
+        (1, "#00FFFF", 10000, 10, lambda v: ("Sessions: %d") % v),
+    ]:        val = float(perf_data[i][1])
+        h += perfometer_logarithmic(val, base, scale, color)
+        texts.append(verbfunc(val))
+    h += '</div>'
+    return " / ".join(texts), h
 
 perfometers["check_mk-winnfssrv"] = perfometer_winnfssrv
